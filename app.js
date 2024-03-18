c65fd88e8ca44cda78723471a764db8c73500309
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-19 01:18:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let personName = `\n\t Zaid Khan\t\n`;
+console.log(personName);
+let stripped = personName.trim();
+console.log(stripped);
---
+++ app.ts
@@ -0,0 +1,4 @@
+// let personName =`\n\t Zaid Khan\t\n`;
+// console.log(personName);
+// let stripped: string = personName.trim();
+// console.log(stripped);
